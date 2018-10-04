9dffe816a7db4298d5141f7ff44cf3186017c6ec
Desmond Rawls <captaingrover@gmail.com>
2018-10-03 22:06:55 -0700

    Make arguments to quantities explicit.

--- src/quantities-tests.purs
+++ src/quantities-tests.purs
@@ -13,17 +13,17 @@ empty = [(Tuple 1 1)]
 quantitiesTests :: Array (Either String String)
 quantitiesTests = [
   it "empty" $ 
-      equals [] (quantities 1 1 1 1 []),
+      equals [] (quantities { size: 1, padding: 1, limit: 1, randomness: 1 } []),
   it "with a radius of zero" $ 
-      equals 2 (length $ quantities 0 0 3 5 [(Tuple 2 2), (Tuple 4 4)]),
+      equals 2 (length $ quantities { size: 0, padding: 0, limit: 3, randomness: 5 } [(Tuple 2 2), (Tuple 4 4)]),
   it "limit one" $ 
-      equals 1 (length $ quantities 1 0 2 3 [(Tuple 2 2), (Tuple 4 4)]),
+      equals 1 (length $ quantities { size: 1, padding: 0, limit: 2, randomness: 3 } [(Tuple 2 2), (Tuple 4 4)]),
   it "with a domain of one" $ 
-      equals [(Tuple 100 100), (Tuple 2 2)] (quantities 1 0 3 5 [(Tuple 2 2)]),
+      equals [(Tuple 100 100), (Tuple 2 2)] (quantities { size: 1, padding: 0, limit: 3, randomness: 5 } [(Tuple 2 2)]),
   it "with a domain less than the requested quantity" $ 
-      equals 2 (length $ quantities 1 0 5 9 [(Tuple 2 2), (Tuple 4 4), (Tuple 6 6)]),
+      equals 2 (length $ quantities { size: 1, padding: 0, limit: 5, randomness: 9 } [(Tuple 2 2), (Tuple 4 4), (Tuple 6 6)]),
   it "padding" $ 
-      equals [(Tuple 6 6)] (quantities 1 2 2 3 [(Tuple 2 2), (Tuple 4 4), (Tuple 6 6), (Tuple 8 8)]),
+      equals [(Tuple 6 6)] (quantities { size: 1, padding: 2, limit: 2, randomness: 3 } [(Tuple 2 2), (Tuple 4 4), (Tuple 6 6), (Tuple 8 8)]),
   it "too much padding returns default" $ 
-      equals [(Tuple 100 100)] (quantities 1 8 2 3 [(Tuple 2 2), (Tuple 4 4), (Tuple 6 6), (Tuple 8 8)])
+      equals [(Tuple 100 100)] (quantities { size: 1, padding: 8, limit: 2, randomness: 3 } [(Tuple 2 2), (Tuple 4 4), (Tuple 6 6), (Tuple 8 8)])
 ]
