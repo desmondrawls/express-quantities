b2e14101d4e85ff00e9458c3c1e32aae63c5288c
Desmond Rawls <captaingrover@gmail.com>
2018-10-03 21:06:36 -0700

    Clean up spacing in the quantities test.

--- src/matchers.purs
+++ src/matchers.purs
@@ -1,15 +1,22 @@
-module Matchers (equals) where
+module Matchers (it, equals) where
 
 import Prelude
 import Effect.Console 
 import Data.Either
+import Data.Maybe
+import Data.Tuple
 import Data.Array
 import Effect (Effect)
 import Data.Foldable (for_)
 
-equals :: forall a. Eq a => Show a => String -> a -> a -> Either String String
-equals name expected actual =
-  if expected == actual then Right $ "passed: " <> name <> "\n"
-  else Left $ "failed: " <> name <> "\n expected: " <> show expected <>
-       ", got: " <> show actual <> "\n"
+it :: forall a b. Show a => Show b => String -> Maybe (Tuple a b) -> Either String String
+it description Nothing 
+    = Right $ "passed: " <> description
+it description (Just (Tuple expected actual)) 
+    = Left $ "failed: " <> description <> "\n expected: " <> show expected <> ", got: " <> show actual <> "\n"
+ 
+
+equals :: forall a. Eq a => Show a => a -> a -> Maybe (Tuple a a)
+equals expected actual | expected == actual = Nothing
+                       | otherwise          = Just $ Tuple expected actual
 
--- src/quantities-tests.purs
+++ src/quantities-tests.purs
@@ -12,10 +12,18 @@ empty = [(Tuple 1 1)]
 
 quantitiesTests :: Array (Either String String)
 quantitiesTests = [
-  equals "empty" [] (quantities 1 1 1 1 []),
-  equals "with a radius of zero" 2 (length $ quantities 0 0 3 5 [(Tuple 2 2), (Tuple 4 4)]),
-  equals "limit one" 1 (length $ quantities 1 0 2 3 [(Tuple 2 2), (Tuple 4 4)]),
-  equals "with a domain of one" [(Tuple 100 100), (Tuple 2 2)] (quantities 1 0 3 5 [(Tuple 2 2)]),
-  equals "with a domain less than the requested quantity" 2 (length $ quantities 1 0 5 9 [(Tuple 2 2), (Tuple 4 4), (Tuple 6 6)]),
-  equals "limit" 1 (length $ quantities 1 0 2 3 [(Tuple 2 2), (Tuple 4 4), (Tuple 6 6), (Tuple 8 8)])
+  it "empty" $ 
+      equals [] (quantities 1 1 1 1 []),
+  it "with a radius of zero" $ 
+      equals 2 (length $ quantities 0 0 3 5 [(Tuple 2 2), (Tuple 4 4)]),
+  it "limit one" $ 
+      equals 1 (length $ quantities 1 0 2 3 [(Tuple 2 2), (Tuple 4 4)]),
+  it "with a domain of one" $ 
+      equals [(Tuple 100 100), (Tuple 2 2)] (quantities 1 0 3 5 [(Tuple 2 2)]),
+  it "with a domain less than the requested quantity" $ 
+      equals 2 (length $ quantities 1 0 5 9 [(Tuple 2 2), (Tuple 4 4), (Tuple 6 6)]),
+  it "padding" $ 
+      equals [(Tuple 6 6)] (quantities 1 2 2 3 [(Tuple 2 2), (Tuple 4 4), (Tuple 6 6), (Tuple 8 8)]),
+  it "too much padding returns default" $ 
+      equals [(Tuple 100 100)] (quantities 1 8 2 3 [(Tuple 2 2), (Tuple 4 4), (Tuple 6 6), (Tuple 8 8)])
 ]
